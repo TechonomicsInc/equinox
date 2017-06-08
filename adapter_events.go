de8e723618316ff1d38233289abecd7864a6b7ea
Lukas Breuer <lukas.breuer@outlook.com>
2017-06-08 20:54:48 +0200

    Move AdapterEvent docs

--- adapter_events.go
+++ adapter_events.go
@@ -2,41 +2,6 @@ package equinox
 
 //go:generate stringer -type AdapterEvent
 
-// AdapterEvent is an enum that tells the router to continue/stop execution
-// You should always try to ignore "direct" AdapterEvent values and use the bound functions instead.
-// This way the error-handling and Event-Parsing stays in a central place.
-//
-// The easiest usage form is to defer the corresponding panic-handler and call AdapterEvent#Act():
-//
-//     defer AdapterPanicHandler()
-//     r.Dispatch(MY_COOL_EVENT, arg1, arg2, arg...).Act()
-//
-// If you want to have some more control about the panics/errors there is AdapterEvent#ShouldAbort().
-// Just wrap the Dispatch call into an if-clause and return as needed.
-//
-//     if r.Dispatch(MY_COOL_EVENT, arg1, arg2, arg...).ShouldAbort() {
-//         return
-//     }
-//
-// And last but not least you can always throw the Event at a switch clause.
-//
-//     switch r.Dispatch(MY_COOL_EVENT, arg1, arg2, arg...) {
-//     case CONTINUE_EXECUTION, NO_HANDLERS_REGISTERED:
-//         /* Continue execution */
-//         break
-//
-//     case STOP_EXECUTION:
-//         /* Kill execution */
-//         return
-//
-//     case PANIC:
-//         panic("wew i did not see that coming")
-//
-//     }
-//
-// AdapterEvents might be added/removed/changed until equinox reaches a stable release so make sure to add a "default"
-// case that handles unknown Events.
-//
 type AdapterEvent int
 
 const (
