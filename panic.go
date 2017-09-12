4b183e88e357bc0869c03a60ecd7dff59199b193
Lukas Breuer <lukas.breuer@outlook.com>
2017-09-12 22:00:01 +0200

    Relicense from AGPL v3 to EUPL v1.1

--- panic.go
+++ panic.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Subliminal Apps
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * <https://joinup.ec.europa.eu/software/page/eupl>
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations
+ * under the Licence.
+ */
+
 package equinox
 
 // PanicHandler defines a function that the router uses to handle panics.
